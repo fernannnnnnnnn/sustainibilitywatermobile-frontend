cc3ff30e9a30b60ad8b9295fbae9bc315c306de2
ffffffff3rnan <lulabyblueeblackberry@gmail.com>
2025-10-10 15:13:16 +0700

    update 10/10/2025

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,23 +2,16 @@ import axios from "axios";
 
 //API DIISI SESUAI IP DI KOMPUTER
 
-//WIFI HOTSPOT
+//WIFI Modem
 // const API_URL = "http://10.1.51.114:5255/";
 
 //WIFI Astra
-export const API_URL = "http://10.1.5.2:5255/";
+//export const API_URL = "http://10.1.5.2:5255/";
 
 // WIFI Zidan
 //export const API_URL = "http://10.127.212.240:5255/";
 
-//WIFI arn
-//export const API_URL = "http://192.168.0.119:5255/";
-//export const API_URL = "http://192.168.0.119:5255/";
-// WIFI arn
-// const API_URL = "http://10.1.51.114:5255/";
-
-// //WIFI KOST-zz
-//export const API_URL = "http://172.20.10.10:5255/";
+export const API_URL = "https://sia-pt.polytechnic.astra.ac.id/stn-air/";
 
 const apiClient = axios.create({ baseURL: API_URL, timeout: 1000 });
 

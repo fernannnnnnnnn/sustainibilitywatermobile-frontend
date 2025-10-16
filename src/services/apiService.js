8f1cc12316b55e3be0e4d29f834cae88a1b67995
Althaf <128457629+Althafazu@users.noreply.github.com>
2025-10-16 08:53:41 +0700

    implementasi postUserArray dan perbaikan chart di HomeAirTab -Altap

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -6,12 +6,15 @@ import axios from "axios";
 // const API_URL = "http://10.1.51.114:5255/";
 
 //WIFI Astra
-//export const API_URL = "http://10.1.5.2:5255/";
+export const API_URL = "http://10.1.19.29:5255/";
 
 // WIFI Zidan
 //export const API_URL = "http://10.127.212.240:5255/";
 
-export const API_URL = "https://sia-pt.polytechnic.astra.ac.id/stn-air/";
+// export const API_URL = "https://sia-pt.polytechnic.astra.ac.id/stn-air/";
+
+// Wifi Daffa
+// export const API_URL = "http://192.168.1.9:5255/";
 
 const apiClient = axios.create({
   baseURL: API_URL,
@@ -36,3 +39,31 @@ export const postUser = async (param, body = {}) => {
     throw error;
   }
 };
+
+export const postUserArray = async (param, body = {}) => {
+  try {
+    const response = await apiClient.post(`api/${param}`, body);
+    let data = response.data;
+
+    if (typeof data === 'string') {
+      try {
+        data = JSON.parse(data);
+      } catch (parseError) {
+        console.error("Error parsing data:", parseError);
+        return "ERROR";
+      }
+    }
+
+    if (Array.isArray(data)) {
+      return data;
+    } else {
+      console.error("❌ API response is not an array:", data);
+      return "ERROR";
+    }
+  } catch (error) {
+    console.error("❌ API call failed:", error.message);
+    if (error.response) {
+      console.error("Response:", error.response.data);
+    } return "ERROR";
+  }
+}
